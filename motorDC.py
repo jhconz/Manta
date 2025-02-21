7e971128d276634f0bbfabe98ced65cf24f6faba
jhconz <105241091+jhconz@users.noreply.github.com>
2025-02-21 14:29:14 -0600

    Add files via upload

--- motorDC.py
+++ motorDC.py
@@ -1,4 +1,4 @@
-# -*-                                                                          
+                                                                    
 """
 Created on Sat Jul 13 12:36:50 2024
 
@@ -13,25 +13,25 @@ from math import pi
 class motorDC:
     
     def __init__(self,Pins):
-        self.OUT1 = Pins[0]
-        self.OUT2 = Pins[-1]
+        self.Out1 = Pins[0]
+        self.Out2 = Pins[-1]
         self.setGPIO()
         
     def setGPIO(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.OUT1, GPIO.OUT)
-        GPIO.setup(self.OUT2, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.Out1, GPIO.OUT)
+        GPIO.setup(self.Out2, GPIO.OUT)
         
-        
+
 ## Initialize Motors
-Pins_A = [20,21]
+Pins_A = [5,6]
 Motor_A = motorDC(Pins_A)
-Pins_B = [23,24]
+Pins_B = [20,21]
 Motor_B = motorDC(Pins_B)
-Motor_A.OUT1.value = False
-Motor_A.OUT2.value = False
-Motor_B.OUT1.value = False
-Motor_B.OUT2.value = False
+GPIO.output(Motor_A.Out1,GPIO.LOW)
+GPIO.output(Motor_A.Out2,GPIO.LOW)
+GPIO.output(Motor_B.Out1,GPIO.LOW)
+GPIO.output(Motor_B.Out2,GPIO.LOW)
 
 
 ## Actuation Scheme
@@ -42,30 +42,32 @@ lag = 2.0
 
 try:
     ## Half Cycle to start
-    Motor_A.OUT2.value = True # Begin MA CCW
+    GPIO.output(Motor_A.Out2,GPIO.HIGH) # Begin MA CCW
     time.sleep(phase)
-    Motor_B.OUT2.value = True #Begin MB CCW
-    Motor_A.OUT2.value = False #End MA CCW
+    GPIO.output(Motor_B.Out2,GPIO.HIGH) #Begin MB CCW
+    GPIO.output(Motor_A.Out2,GPIO.LOW) # Begin MA CCW #End MA CCW
     time.sleep(phase)
-    Motor_B.OUT2.value = False #End MB CCW
+    GPIO.output(Motor_B.Out2,GPIO.LOW) #End MB CCW
     time.sleep(lag-phase)   
     while True:
-        Motor_A.OUT1.value = True #Begin MA CW
+        GPIO.output(Motor_A.Out1,GPIO.HIGH) # Begin MA CW
         time.sleep(phase)
-        Motor_B.OUT1.value = True #Begin MB CW
+        GPIO.output(Motor_B.Out1,GPIO.HIGH) #Begin MB CW
         time.sleep(t_on-phase)
-        Motor_A.OUT1.value = False #End MA CW
+        GPIO.output(Motor_A.Out1,GPIO.LOW) #End MA CW
         time.sleep(phase)
-        Motor_B.OUT1.value = False #End MB CW
+        GPIO.output(Motor_B.Out1,GPIO.LOW) #End MB CW
         time.sleep(lag-phase)
-        Motor_A.OUT2.value = True # Begin MA CCW
+        GPIO.output(Motor_A.Out2,GPIO.HIGH) # Begin MA CCW
         time.sleep(phase)
-        Motor_B.OUT2.value = True #Begin MB CCW
+        GPIO.output(Motor_B.Out2,GPIO.HIGH) #Begin MB CCW
         time.sleep(t_on-phase)
-        Motor_A.OUT2.value = False #End MA CCW
+        GPIO.output(Motor_A.Out2,GPIO.LOW) #End MA CCW
         time.sleep(phase)
-        Motor_B.OUT2.value = False #End MB CCW
+        GPIO.output(Motor_B.Out2,GPIO.LOW) #End MB CCW
         time.sleep(lag-phase)        
 
 except KeyboardInterrupt:
     print("End Actuation")
+finally:
+    GPIO.cleanup()
